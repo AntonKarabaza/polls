84d022004925029e4d60584a40fbab363a495b5c
Anton Karabaza <antonkarabaza@gmail.com>
2022-09-24 11:42:36 +0200

    5: Add get_questions view

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,15 @@
 # Polls views
 from aiohttp import web
 
+from common.utils import to_json
+from modules.data_service.polls import PollsDataService
+
 
 async def index(request):
     return web.Response(text='Hello Aiohttp!')
+
+
+async def get_questions(request):
+    polls_data_service = PollsDataService(**request.app['config']['db']['postgres'])
+    questions = await polls_data_service.get_questions()
+    return web.Response(text=to_json(questions))
