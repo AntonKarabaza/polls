e311360de6bfebb096d307a3a4c53674a5453306
Anton Karabaza <antonkarabaza@gmail.com>
2022-09-24 11:31:03 +0200

    5: Add PollsDataService implementation

---
+++ app/modules/data_service/polls.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from connectors.db.postgres import PostgresDataService
+
+
+class PollsDataService(PostgresDataService):
+    """Class for data manipulation for polls."""
+    def __init__(
+            self, host: str, port: int, database: str, user: str, password: str, schema: Optional[str] = None
+    ):
+        super().__init__(host, port, database, user, password, schema)
+
+    async def get_questions(self):
+        async with self.transaction() as session:
+            data = await session.execute('SELECT * FROM question;')
+            return tuple({key: val for key, val in row.items()} for row in data.mappings())
+
+    async def get_choices(self):
+        raise NotImplementedError
