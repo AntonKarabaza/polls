6fcfdf60132d35fd21cd41ca60e02f094c9040ca
Anton Karabaza <antonkarabaza@gmail.com>
2022-09-24 11:43:36 +0200

    5: Register get_questions view for route GET /questions

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,8 @@
 from aiohttp import web
 
-from api.views import index
+from api.views import index, get_questions
 
 
 def set_up_routes(app: web.Application):
     app.router.add_get('/', index)
+    app.router.add_get('/questions', get_questions)
