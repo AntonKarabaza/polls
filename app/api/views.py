8c744093b3ec2a1a2e5cf583dbd4713b345a088f
Anton Karabaza <antonkarabaza@gmail.com>
2022-09-25 10:37:37 +0200

    5: Use PollsDataService as singleton in views

--- app/api/views.py
+++ app/api/views.py
@@ -10,6 +10,6 @@ async def index(request):
 
 
 async def get_questions(request):
-    polls_data_service = PollsDataService(**request.app['config']['db']['postgres'])
+    polls_data_service = PollsDataService.get_instance()
     questions = await polls_data_service.get_questions()
     return web.Response(text=to_json(questions))
